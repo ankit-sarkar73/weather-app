3e13392278087a43a0c20818aeba00ac096fff98
ankit-sarkar73 <154975368+ankit-sarkar73@users.noreply.github.com>
2024-08-30 20:26:05 +0530

    Update script.js

--- weather-app/frontend/script.js
+++ weather-app/frontend/script.js
@@ -2,12 +2,14 @@ document.getElementById('weather-form').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const city = document.getElementById('city').value;
-    fetch('/get_weather', {
-        method: 'POST',
+    console.log(city)
+    const url = `http://127.0.0.1:5000/get_weather?city=${encodeURIComponent(city)}`;
+
+    fetch(url, {
+        method: 'GET',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: 'city=' + city
+        }
     })
     .then(response => response.json())
     .then(data => {
